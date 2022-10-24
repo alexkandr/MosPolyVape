f1ba59b71a1eda4094b5d29a8616e6b424c85ce3
Александр Акантьев <akantyevad@gmail.com>
2022-10-25 02:51:09 +0300

    changed python version to 3.11

--- Bot.py
+++ Bot.py
@@ -3,6 +3,14 @@ from telebot import types
 
 bot = telebot.TeleBot('5612788804:AAF5Qf8SC1yLjinjGsnGGaDbhI39drR1Uw4')
 
+@bot.callback_query_handler(func=lambda call: True)
+def callback_handler(call):
+    match call.data:
+        case 'купить':
+            bot.register_callback_query_handler()
+        case _:
+            bot.send_message(text='что-то пошло не так')
+
 @bot.message_handler(content_types=['text'])
 def main_menu(message):
     keyboard = types.InlineKeyboardMarkup()
@@ -11,12 +19,23 @@ def main_menu(message):
 
     key_back = types.InlineKeyboardButton(text='Назад', callback_data='back')
     keyboard.add(key_back)
+
+    key_cart = types.InlineKeyboardButton(text='Корзина', callback_data='cart')
+    keyboard.add(key_cart)
+
+    key_addres = types.InlineKeyboardButton(text='Ну типа адрес', callback_data='addres')
+    keyboard.add(key_cart)
+
+    key_contact = types.InlineKeyboardButton(text='Связь', callback_data='contact')
+    keyboard.add(key_contact)
     
     bot.send_message(message.from_user.id, 
         text = 'Это бот для покупки парилок в общаге. feel free to contact us^ enjoy the ПЫХ', 
         reply_markup=keyboard)
 
 
-bot.polling(none_stop=True, interval=0)
+
+
+bot.infinity_polling()
 
 
